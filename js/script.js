663d018eceb3fe5d6745ab09816a21ca4ff32937
Максим <wintry.wisper@mail.ru>
2022-10-18 16:16:46 +0300

    object>img-card

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('.form');
+const input= document.querySelector('.form_input');
+
+input.addEventListener('focus',function (){
+    form.classList.add('form--active');
+
+})
+input.addEventListener('blur',function (){
+    form.classList.remove('form--active');
+
+})
+//стилизация кнопки(подсвечивание)
